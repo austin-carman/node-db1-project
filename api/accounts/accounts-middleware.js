ae510f201e12908bddf8daf3b7fbe69ebb201bcd
Austin Carman <abcarman12@gmail.com>
2021-06-07 20:55:35 -0600

    completed all mvp

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,63 @@
-exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+const Accounts = require('./accounts-model');
+
+
+const checkAccountPayload = (req, res, next) => { 
+  if (!req.body.name || !('budget' in req.body)) {
+    res.status(400).json({ 
+      message: 'name and budget are required' 
+    });
+  } else if (typeof(req.body.name) != 'string') {
+    res.status(400).json({ 
+      message: 'name of account must be a string' 
+    });
+  } else if (req.body.name.trim().length < 3 || req .body.name.trim().length > 100) {
+    res.status(400).json({ 
+      message: 'name of account must be be between 3 and 100' 
+    });
+  } else if (typeof(req.body.budget) != 'number') {
+    res.status(400).json({ 
+      message: 'budget of account must be a number' 
+    });
+  } else if (req.body.budget < 0 || req.body.budget > 1000000) {
+    res.status(400).json({ 
+      message: "budget of account is too large or too small" 
+    })
+  } else {
+    req.account = { name: req.body.name.trim(), budget: req.body.budget }
+    next()
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkAccountNameUnique = (req, res, next) => {
+  Accounts.getAll()
+    .then(accounts => {
+      if (accounts.filter(account => account.name == req.body.name).length > 0) {
+        res.status(400).json({ 
+          message: 'that name is taken'
+        })
+      } else {
+        next()
+      }
+    })
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkAccountId = (req, res, next) => {
+  Accounts.getById(req.params.id)
+    .then(account => {
+      if (!account) {
+        res.status(404).json({
+          message: 'Account not found'
+        })
+      } else {
+        req.account = account
+        next()
+      }
+    })
+    .catch(next);
+}
+
+module.exports = {
+  checkAccountPayload,
+  checkAccountNameUnique,
+  checkAccountId
 }
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,37 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  // return db.select('*').from('accounts');
+  return db('accounts');
 }
 
-const getById = id => {
-  // DO YOUR MAGIC
+const getById = async (id) => {
+  const result = await db('accounts')
+    .where('id', id).first()
+
+    return result
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async ({ name, budget }) => {
+  const [id] = await db('accounts')
+  .insert({ name, budget })
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  await db('accounts')
+  .where('id', id)
+  .update({name: account.name, budget: account.budget})
+  return getById(id)
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+const deleteById = async (id) => {
+  const toRemove = await getById(id)
+  await db('accounts')
+    .where({ id })
+    .del()
+
+    return toRemove;
 }
 
 module.exports = {
